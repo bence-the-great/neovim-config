b9a0c39e4549480e35074f390274bf1dfbe027f8
bence-the-great <bence.szabo.92@gmail.com>
2023-09-05 23:49:28 +0200

    feat(keymap): add window navigation

--- lua/configs/remap.lua
+++ lua/configs/remap.lua
@@ -2,3 +2,18 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>q", vim.cmd.q)
 
+-- Movements between windows
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- Resize windows
+vim.keymap.set("n", "<C-Up>", "<cmd>resize -2<cr>")
+vim.keymap.set("n", "<C-Down>", "<cmd>resize +2<cr>")
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>")
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>")
+
+-- Clear highlight
+vim.keymap.set("n", "<leader>c", "<cmd>nohl<cr>")
+
