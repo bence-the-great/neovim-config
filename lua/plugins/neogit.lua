beb43b0dc7652b371135d7a767da5459dcb6763b
bence-the-great <bence.szabo.92@gmail.com>
2023-09-05 23:50:31 +0200

    feat(keymap): add neogit shortcuts

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -7,5 +7,9 @@ return {
   },
   config = true,
   event = "VeryLazy",
+  keys = {
+    { "<leader>Gg", "<cmd>Neogit<cr>", desc = "Open Neogit" },
+    { "<leader>Gc", "<cmd>Neogit commit<cr>", desc = "Git commit" },
+  },
 }
 
