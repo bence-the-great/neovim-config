64016e807b24d3e18e4cd3aa64193b3cc5358f2f
bence-the-great <bence.szabo.92@gmail.com>
2023-09-05 23:15:07 +0200

    feat(plugins): add noice

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,10 @@ return {
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
     { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find files in git" },
     { "<leader>fl", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+    { "<leader>fn", "<cmd>Telescope noice<cr>", desc = "Find notification" },
   },
+  config = function (_, opts)
+    require("telescope").load_extension("noice")
+  end,
 }
 
