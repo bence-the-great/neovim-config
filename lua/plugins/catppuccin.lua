8a93691a0c05d9c38e0d0ff50bad4580520be3f9
bence-the-great <bence.szabo.92@gmail.com>
2023-09-06 14:01:36 +0200

    feat(plugin): add catppuccin integrations
    Also, turn off transparent background

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,11 +4,15 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      transparent_background = true,
+      -- transparent_background = true,
       show_end_of_buffer = true,
       integrations = {
         treesitter = true,
         neotree = true,
+        gitsigns = true,
+        noice = true,
+        telescope = true,
+        neogit = true,
       },
     }); 
     vim.cmd([[colorscheme catppuccin]])
