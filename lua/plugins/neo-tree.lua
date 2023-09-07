e235c9ccba093f649631715804b5ad4b24f9afea
bence-the-great <bence.szabo.92@gmail.com>
2023-09-07 08:55:59 +0200

    feat(plugin): finetune neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,35 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
+  cmd = "Neotree",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  event = "VeryLazy",
   keys = {
     { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle Neotree" },
+  },
+  opts = {
+    auto_clean_after_session_restore = true,
+    sources = { "filesystem", "buffers", "git_status" },
+    window = {
+      width = 40,
+      position = "left",
+    },
+    filesystem = {
+      follow_current_file = { enabled = true },
+      hijack_netrw_behavior = "open_current",
+      use_libuv_file_watcher = true,
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".git",
+        },
+      },
+    },
   },
 }
 
