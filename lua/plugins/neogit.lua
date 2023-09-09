fa7fe8d708854e18e308b3d7ef05dc3f536002d7
bence-the-great <bence.szabo.92@gmail.com>
2023-09-09 12:34:35 +0200

    fix: empty whitespaces

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,9 +1,9 @@
 return {
   "NeogitOrg/neogit",
   dependencies = {
-    "nvim-lua/plenary.nvim",        
+    "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
-    "sindrets/diffview.nvim",       
+    "sindrets/diffview.nvim",
   },
   config = true,
   event = "VeryLazy",
