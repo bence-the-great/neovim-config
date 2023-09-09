21fdb29a50089f196d82e60fe8fbde22c379d8ce
bence-the-great <bence.szabo.92@gmail.com>
2023-09-09 11:58:42 +0200

    feat(catppuccin): enable more integrations

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -17,8 +17,32 @@ return {
           enabled = true,
           colored_indent_levels = true,
         },
+        illuminate = {
+          enabled = true,
+          lsp = false
+        },
+        which_key = true,
+        cmp = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
       },
-    }); 
+    });
     vim.cmd([[colorscheme catppuccin]])
   end,
 }
