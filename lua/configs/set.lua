d11c44d0417ef47b8cb532a7d550c56a6aca355e
bence-the-great <bence.szabo.92@gmail.com>
2023-09-09 23:02:46 +0200

    feat(options): add options from kickstart.nvim

--- lua/configs/set.lua
+++ lua/configs/set.lua
@@ -1,9 +1,22 @@
+vim.o.termguicolors = true
+
 vim.wo.number = true
 vim.wo.relativenumber = true
 
 vim.o.expandtab = true -- Pressing the <tab> key will insert spaces instead of a <tab> character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a <tab> character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- Search
+vim.o.hlsearch = false
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Enable mouse mode
+vim.o.mouse = true
+
+-- Save undo history
+vim.o.undofile = true
 
 -- lualine
 vim.opt.showmode = false
