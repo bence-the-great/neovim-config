4da35a888d2f1651e2d1fdca57b2165b3e62e13b
bence-the-great <bence.szabo.92@gmail.com>
2023-09-09 12:34:18 +0200

    feat(plugin): add hlslens

---
+++ lua/plugins/hlslens.lua
@@ -0,0 +1,16 @@
+return {
+  "kevinhwang91/nvim-hlslens",
+  keys = {
+    { "c", "*<cmd>noh<cr>", desc = "" },
+    { "*", "*<cmd>lua require('hlslens').start()<cr>", desc = "" },
+    { "g*", "g*<cmd>lua require('hlslens').start()<cr>", desc = "" },
+    { "#", "#<cmd>lua require('hlslens').start()<cr>", desc = "" },
+    { "g#", "g#<cmd>lua require('hlslens').start()<cr>", desc = "" },
+    { "n", "<cmd>execute('normal! ' . v:count1 . 'n')<cr><cmd>lua require('hlslens').start()<cr>", desc = "" },
+    { "N", "<cmd>execute('normal! ' . v:count1 . 'N')<cr><cmd>lua require('hlslens').start()<cr>", desc = "" },
+  },
+  opts = {
+    calm_down = true,
+  },
+}
+
