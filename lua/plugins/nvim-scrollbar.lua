45ac8e2058c059fee204483e08a8af35046ea3b3
bence-the-great <bence.szabo.92@gmail.com>
2023-09-09 11:55:32 +0200

    feat(plugin): add scrollbar

---
+++ lua/plugins/nvim-scrollbar.lua
@@ -0,0 +1,29 @@
+return {
+  "petertriho/nvim-scrollbar",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    handle = {
+      color = "#666666",
+      hide_if_all_visible = false,
+    },
+    handlers = {
+      cursor = true,
+      diagnostic = true,
+      gitsigns = true,
+      handle = true,
+      search = false, -- Requires hlslens
+      ale = false, -- Requires ALE
+    },
+    excluded_filetypes = {
+        "cmp_docs",
+        "cmp_menu",
+        "noice",
+        "notify",
+        "prompt",
+        "TelescopePrompt",
+        "neo-tree",
+        "NeogitStatus",
+    },
+  },
+}
+
