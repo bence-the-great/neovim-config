3240c0736b47621c5d0b7cbaeed38a0788516898
bence-the-great <bence.szabo.92@gmail.com>
2023-09-14 21:56:51 +0200

    fix(treesitter): add config call
    Also, enable indenting, and incremental selection.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,11 +14,26 @@ return {
         local max_filesize = 100 * 1024 -- 100 KB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
-            return true
+          return true
         end
       end,
       additional_vim_regex_highlighting = false,
     },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "gnn", -- set to `false` to disable one of the mappings
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+      },
+    },
+    indent = {
+      enabled = true,
+    },
   },
+  config = function (_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
 
