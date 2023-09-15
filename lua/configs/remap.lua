87ab8dfdb32b2175af0bbf4f363c1631391a4900
bence-the-great <bence.szabo.92@gmail.com>
2023-09-15 16:33:18 +0200

    feat(shortcut): window actions for toggleterm

--- lua/configs/remap.lua
+++ lua/configs/remap.lua
@@ -6,10 +6,18 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h")
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j")
+vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k")
+vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l")
 
 -- Resize windows
 vim.keymap.set("n", "<C-Up>", "<cmd>resize -2<cr>")
 vim.keymap.set("n", "<C-Down>", "<cmd>resize +2<cr>")
 vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>")
 vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>")
+vim.keymap.set("t", "<C-Up>", "<C-\\><C-n><cmd>resize -2<cr>")
+vim.keymap.set("t", "<C-Down>", "<C-\\><C-n><cmd>resize +2<cr>")
+vim.keymap.set("t", "<C-Left>", "<C-\\><C-n><cmd>vertical resize -2<cr>")
+vim.keymap.set("t", "<C-Right>", "<C-\\><C-n><cmd>vertical resize +2<cr>")
 
