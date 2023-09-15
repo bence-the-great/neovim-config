29c258357fa959daa6c3c22ce6772550270d4e5c
bence-the-great <bence.szabo.92@gmail.com>
2023-09-15 20:51:14 +0200

    feat(plugin): search for selected text

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,20 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.2',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     { "<leader>ft", "<cmd>Telescope<cr>", desc = "Open Telescope" },
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
     { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find files in git" },
     { "<leader>fl", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+    {
+      "<leader>fl",
+      function ()
+        vim.cmd("noautocmd normal! \"vy\"")
+        require("telescope.builtin").live_grep({default_text = vim.fn.getreg("v")})
+      end,
+      mode = "v",
+      desc = "Live grep selection",
+    },
     { "<leader>fn", "<cmd>Telescope noice<cr>", desc = "Find notification" },
   },
   config = function (_, opts)
