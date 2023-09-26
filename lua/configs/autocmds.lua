886884e8b2adebfea96fed2c16b760f33a8c0d04
bence-the-great <bence.szabo.92@gmail.com>
2023-09-26 17:37:14 +0200

    fix(git): proper commit message column widths
    At 50 and 72 characters. Github handles 72 chars without wrapping, but
    generally, the 50-char limit is suggested for commit message titles.

--- lua/configs/autocmds.lua
+++ lua/configs/autocmds.lua
@@ -4,7 +4,7 @@ vim.api.nvim_create_augroup("git", {})
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "*COMMIT_EDITMSG",
   callback = function()
-    vim.api.nvim_set_option_value("colorcolumn", "50,80", {})
+    vim.api.nvim_set_option_value("colorcolumn", "50,72", {})
   end,
   group = "git",
 })
