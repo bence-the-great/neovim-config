d28a16e1cc0d6a2e96dba5b004510a8c23cedec0
bence-the-great <bence.szabo.92@gmail.com>
2023-10-05 11:16:50 +0200

    feat(telescope): add fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,13 @@ end
 
 return {
   "nvim-telescope/telescope.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+    }
+  },
   keys = {
     { "<leader>ft", "<cmd>Telescope<cr>", desc = "Open Telescope" },
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
@@ -31,11 +37,22 @@ return {
     { "<leader>fn", "<cmd>Telescope noice<cr>", desc = "Find notification" },
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffer" },
   },
+  opts = {
+    extensions = {
+      fzf = {
+        fuzzy = true,                    -- false will only do exact matching
+        override_generic_sorter = true,  -- override the generic sorter
+        override_file_sorter = true,     -- override the file sorter
+        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+      }
+    }
+  },
   config = function (_, opts)
     local telescope = require("telescope")
     telescope.setup(opts)
     telescope.load_extension("noice")
     telescope.load_extension("projects")
+    telescope.load_extension("fzf")
   end,
 }
 
