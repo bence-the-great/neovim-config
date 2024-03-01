94e941dd59bf42ea518c8deca2c922aa7f408a92
bence-the-great <bence.szabo.92@gmail.com>
2024-03-01 20:22:11 +0100

    feat(harpoon, gitsigns): modify key bindings

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -11,5 +11,19 @@ return {
       untracked    = { text = '┆' },
     },
     current_line_blame = true,
+    on_attach = function(client, bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      map('n', '<leader>tb', gs.toggle_current_line_blame)
+
+      map('n', '<leader>hp', gs.preview_hunk)
+      map('n', '<leader>hr', gs.reset_hunk)
+    end
   },
 }
