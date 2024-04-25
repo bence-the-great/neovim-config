7d978edb2c5ac463ba18143be22d188d233ad3f3
bence-the-great <bence.szabo.92@gmail.com>
2024-04-25 22:15:50 +0200

    fix(project): only use .git to detect project root

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,7 +1,11 @@
 return {
   "ahmedkhalf/project.nvim",
   config = function(_, opts)
-    require("project_nvim").setup(opts)
+    require("project_nvim").setup({
+      show_hidden = true,
+      detection_methods = { "pattern" },
+      patterns = {".git"},
+    })
   end,
 }
 
