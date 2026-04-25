713891b1ae72dafea64aeb170947651f7cb18ac0
Bence Szabó <bence.szabo.92@gmail.com>
2026-04-25 09:38:24 +0200

    feat: remove github copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -21,33 +21,4 @@ return {
       })
     end,
   },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "main",
-    dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-    },
-    opts = {
-      show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
-      debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
-      disable_extra_info = 'no', -- Disable extra information (e.g: system prompt) in the response.
-      language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
-      -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
-      -- temperature = 0.1,
-      mappings = {
-        reset = {
-          normal ='<C-r>',
-          insert = '<C-r>'
-        }
-      }
-    },
-    build = function()
-      vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
-    end,
-    event = "VeryLazy",
-    keys = {
-      { "<leader>ccc", ":CopilotChat ", desc = "CopilotChat - Ask something" },
-    },
-  },
 }
